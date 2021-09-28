fd773ece994d2af637fdf4cdae6416a528636e7f
Danny Ruiz <ynnadrules@gmail.com>
2021-09-28 08:21:02 -0500

    Support branch for AVManager

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "AbsintheSocketTransport",
-    platforms: [.iOS(.v12), .macOS(.v10_14), .tvOS(.v12), .watchOS(.v5)],
+    platforms: [.iOS(.v13), .macOS(.v10_14), .tvOS(.v12), .watchOS(.v5)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -13,8 +13,10 @@ let package = Package(
             targets: ["AbsintheSocketTransport"]),
     ],
     dependencies: [
-      .package(url: "https://github.com/davidstump/SwiftPhoenixClient.git", from: "1.3.0"),
-      .package(name: "Apollo", url: "https://github.com/apollographql/apollo-ios.git", .upToNextMinor(from: "0.41.0")),
+//        .package(url: "https://github.com/davidstump/SwiftPhoenixClient.git", from: "1.3.0"),
+//        .package(name: "SwiftPhoenixClient", path: "../SwiftPhoenixClient"),
+        .package(url: "https://github.com/avmanager/SwiftPhoenixClient.git", .branch("feature/better-url-param-encoding")),
+        .package(name: "Apollo", url: "https://github.com/apollographql/apollo-ios.git", .upToNextMinor(from: "0.48.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
